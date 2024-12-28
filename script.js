c535f233eef9556704306b4e5990b803a6b35f0d
Rio-Rajkumar <rioraj1359@gmail.com>
2024-12-28 18:00:45 +0530

    project1

---
+++ script.js
@@ -0,0 +1,47 @@
+// // counting
+
+// var count1 = document.getElementById('count1')
+// var count = 1
+
+// function counting(){
+//      if(count<=65){
+//     count1.textContent = count
+//     count++
+//  }
+// }
+//  setInterval(counting,400)
+
+// //  
+// var count2 = document.getElementById('count2')
+// var countnum = 1
+
+// function countingnum(){
+//     if(countnum<=58){
+//         count2.textContent = countnum
+//         countnum++
+//     }
+// }
+// setInterval(countingnum,400)
+
+// //
+// var count3 =document.getElementById('count3')
+// var number = 1
+ 
+// function numbercount(){
+//     if(number<=12){
+//         count3.textContent = number
+//         number++
+//     }
+// }
+// setInterval(numbercount,400)
+// //
+
+// var count4 =document.getElementById('count4')
+// var number1 = 1
+// function numbercount1(){
+//     if(number1<=16){
+//         count4.textContent = number1
+//         number1++
+//     }
+// }
+//  setInterval(numbercount1,400)
